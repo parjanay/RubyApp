e8405652f080abc43b212b86c48a8e76e53a2c58
Parjanay Sharma <parjanay.sharma@avaamo.com>
2022-08-02 18:50:01 +0530

    Added Articles Migrate file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_02_084738) do
+ActiveRecord::Schema.define(version: 2022_08_02_131652) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"

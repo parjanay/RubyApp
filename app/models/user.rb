7f997841d5efec9313c59adba60bbd5f20916985
Parjanay Sharma <parjanay.sharma@avaamo.com>
2022-08-04 04:46:25 +0530

    User validatioins added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+    validates :name , 
+                presence: true, 
+                length: {minimum: 3, maximum: 25}, 
+                uniqueness: {case_sensitive: false}
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email , 
+                presence: true, 
+                length: {maximum: 25}, 
+                uniqueness: {case_sensitive: false},
+                format: {with: VALID_EMAIL_REGEX}
 end

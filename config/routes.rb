2a982aa9ab7d95e2c264f92890c025f449765eaf
Parjanay Sharma <parjanay.sharma@avaamo.com>
2022-08-03 17:00:52 +0530

    Added functionality to add new artices through browser

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   #resources :users
-  resources :articles, only: [:show, :index]
+  resources :articles, only: [:show, :index, :create, :new]
   root 'pages#home'
   get 'about',to: 'pages#about'
 end

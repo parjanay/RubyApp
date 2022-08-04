2a6ac7bc55cd7e393a5779e136f6f80e3d34605f
Parjanay Sharma <parjanay.sharma@avaamo.com>
2022-08-04 13:00:41 +0530

    Added Signup feature to app

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  #resources :users
   resources :articles
+  resources :users, except: [:new]
   root 'pages#home'
   get 'about',to: 'pages#about'
+  get 'signup', to: 'users#new'
 end
